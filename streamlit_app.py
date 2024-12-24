2eefd2f4ef1995fd97f4ea691aefee8dcef60a68
Nguyen Van Nhuan <45336894+nhuannguyen1@users.noreply.github.com>
2024-12-24 18:27:05 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,13 @@ from openai import OpenAI
 # Show title and description.
 st.title("💬 Chatbot")
 st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+    "Đây là trợ lý của Nhuần xin chào nguyễn văn Nhuần")
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_key = "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"
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
@@ -54,3 +52,4 @@ else:
         with st.chat_message("assistant"):
             response = st.write_stream(stream)
         st.session_state.messages.append({"role": "assistant", "content": response})
+
